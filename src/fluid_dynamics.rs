2dc281b28cc989145e7f8a3c2f09bc02dffa7a0a
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-04-12 20:01:00 +0200

    Move Fluid struct & fix borrow errors

--- src/fluid_dynamics.rs
+++ src/fluid_dynamics.rs
@@ -1,34 +1,84 @@
 use crate::*;
-use graphics::types::*;
 
+// Dimensions of simulation
 const SIM_WIDTH: usize = (WINDOW_WIDTH / PIXEL_SCALE) as usize;
 const SIM_HEIGHT: usize = (WINDOW_HEIGHT / PIXEL_SCALE) as usize;
 
-pub struct FluidProperties {
-    velocity: [[Vec2d; SIM_WIDTH]; SIM_HEIGHT],
-    force: [[Vec2d; SIM_WIDTH]; SIM_HEIGHT],
-    pressure: f64,
-    density: f64,
-    viscocity: f64,
+pub struct Fluid {
+    pub density: Array2<f64>,
+    velocity_x: Array2<f64>,
+    velocity_y: Array2<f64>,
+    diffusion: f64,
+    viscosity: f64,
 }
 
-impl FluidProperties {
-    // Starting state of simulation
-    pub fn new() -> Self {
-        FluidProperties {
-            velocity: [[[0.0, 0.0]; SIM_WIDTH]; SIM_HEIGHT],
-            force: [[[0.0, 0.0]; SIM_WIDTH]; SIM_HEIGHT],
-            pressure: 1.0,
-            density: 1.0,
-            viscocity: 1.0,
+impl Fluid {
+    pub fn new(diffusion: f64, viscosity: f64) -> Self {
+        Fluid {
+            density: Array::zeros((SIM_WIDTH, SIM_HEIGHT)),
+            velocity_x: Array::zeros((SIM_WIDTH, SIM_HEIGHT)),
+            velocity_y: Array::zeros((SIM_WIDTH, SIM_HEIGHT)),
+            diffusion,
+            viscosity,
+        }
+    }
+
+    pub fn add_density(&mut self, x: usize, y: usize, amount: f64) {
+        self.density[[x, y]] += amount;
+    }
+
+    pub fn add_velocity(&mut self, x: usize, y: usize, amount_x: f64, amount_y: f64) {
+        self.velocity_x[[x, y]] += amount_x;
+        self.velocity_y[[x, y]] += amount_y;
+    }
+
+    pub fn step(&mut self, dt: f64) {
+        let velocity_x0 = self.velocity_x.clone();
+        let velocity_y0 = self.velocity_y.clone();
+        let density0 = self.density.clone();
+    
+        diffuse(&mut self.velocity_x, &velocity_x0, self.viscosity, dt);
+        diffuse(&mut self.velocity_y, &velocity_y0, self.viscosity, dt);
+    
+        advect(&mut self.velocity_x, &velocity_x0, &velocity_x0, &velocity_y0, dt);
+        advect(&mut self.velocity_y, &velocity_y0, &velocity_x0, &velocity_y0, dt);
+    
+        diffuse(&mut self.density, &density0, self.diffusion, dt);
+        advect(&mut self.density, &density0, &self.velocity_x, &self.velocity_y, dt);
+    }
+}
+
+fn lin_solve(x: usize, y: usize, a: f64, c: f64, b: &mut Array2<f64>, x_prev: &Array2<f64>) {
+    let a_inv = 1.0 / (1.0 + 4.0 * a);
+    let iterations = 20; //can be changed depending on need
+
+    for _ in 0..iterations {
+        for j in 1..SIM_HEIGHT - 1 {
+            for i in 1..SIM_WIDTH - 1 {
+                b[[i, j]] = (x_prev[[i, j]] + a * (b[[i - 1, j]] + b[[i + 1, j]] + b[[i, j - 1]] + b[[i, j + 1]])) * a_inv;
+            }
         }
     }
 }
 
-pub fn fluid_dynamics(x: u32, y: u32, fluid_properties: &FluidProperties) -> image::Rgba<u8> {
-    let mut new_color = image::Rgba([0, 0, 0, 255]);
+fn diffuse(b: &mut Array2<f64>, x: &Array2<f64>, a: f64, dt: f64) {
+    let a = dt * a * (SIM_WIDTH - 2) as f64 * (SIM_HEIGHT - 2) as f64;
+    lin_solve(1, 1, a, 1.0 + 4.0 * a, b, x);
+}
 
-    
+fn advect(d: &mut Array2<f64>, d0: &Array2<f64>, veloc_x: &Array2<f64>, veloc_y: &Array2<f64>, dt: f64) {
+    let dt0_x = dt * (SIM_WIDTH - 2) as f64;
+    let dt0_y = dt * (SIM_HEIGHT - 2) as f64;
 
-    return new_color;
+    for j in 1..SIM_HEIGHT - 1 {
+        for i in 1..SIM_WIDTH - 1 {
+            let x = (i as f64 - dt0_x * veloc_x[[i, j]]) as usize;
+            let y = (j as f64 - dt0_y * veloc_y[[i, j]]) as usize;
+
+            let x = x.max(1).min(SIM_WIDTH - 2);
+            let y = y.max(1).min(SIM_HEIGHT - 2);
+
+            d[[i, j]] = d0[[x, y]];
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,14 +4,11 @@ extern crate opengl_graphics;
 extern crate piston;
 extern crate image;
 extern crate ndarray;
-//added
-use ndarray::{Array, Array2};
-
-
 
 mod fluid_dynamics;
 
 use fluid_dynamics::*;
+use ndarray::{Array, Array2};
 use glutin_window::GlutinWindow as Window;
 use opengl_graphics::{GlGraphics, OpenGL, Texture, TextureSettings, Filter};
 use piston::event_loop::{EventSettings, Events};
@@ -30,95 +27,6 @@ pub struct App {
     time: f64,  // Time progressed since start
     mouse_pos: [f64; 2],
     fluid: Fluid,
-}
-
-// Fluid struct
-pub struct Fluid {
-    density: Array2<f64>,
-    velocity_x: Array2<f64>,
-    velocity_y: Array2<f64>,
-    width: usize,
-    height: usize,
-    diffusion: f64,
-    viscosity: f64,
-}
-
-// Fluid methods
-impl Fluid {
-    pub fn new(width: usize, height: usize, diffusion: f64, viscosity: f64) -> Self {
-        Fluid {
-            density: Array::zeros((width, height)),
-            velocity_x: Array::zeros((width, height)),
-            velocity_y: Array::zeros((width, height)),
-            width,
-            height,
-            diffusion,
-            viscosity,
-        }
-    }
-
-    pub fn add_density(&mut self, x: usize, y: usize, amount: f64) {
-        self.density[[x, y]] += amount;
-    }
-
-    pub fn add_velocity(&mut self, x: usize, y: usize, amount_x: f64, amount_y: f64) {
-        self.velocity_x[[x, y]] += amount_x;
-        self.velocity_y[[x, y]] += amount_y;
-    }
-
-    fn lin_solve(&mut self, x: usize, y: usize, a: f64, c: f64, b: &mut Array2<f64>, x_prev: &Array2<f64>) {
-        let a_inv = 1.0 / (1.0 + 4.0 * a);
-        let width = self.width;
-        let height = self.height;
-        let iterations = 20; //can be changed depending on need
-
-        for _ in 0..iterations {
-            for j in 1..height - 1 {
-                for i in 1..width - 1 {
-                    b[[i, j]] = (x_prev[[i, j]] + a * (b[[i - 1, j]] + b[[i + 1, j]] + b[[i, j - 1]] + b[[i, j + 1]])) * a_inv;
-                }
-            }
-        }
-    }
-
-    fn diffuse(&mut self, b: &mut Array2<f64>, x: &mut Array2<f64>, a: f64, dt: f64) {
-        let a = dt * a * (self.width - 2) as f64 * (self.height - 2) as f64;
-        self.lin_solve(1, 1, a, 1.0 + 4.0 * a, b, x);
-    }
-
-    fn advect(&mut self, d: &mut Array2<f64>, d0: &Array2<f64>, veloc_x: &Array2<f64>, veloc_y: &Array2<f64>, dt: f64) {
-        let width = self.width;
-        let height = self.height;
-
-        let dt0_x = dt * (width - 2) as f64;
-        let dt0_y = dt * (height - 2) as f64;
-        for j in 1..height - 1 {
-            for i in 1..width - 1 {
-                let x = (i as f64 - dt0_x * veloc_x[[i, j]]) as usize;
-                let y = (j as f64 - dt0_y * veloc_y[[i, j]]) as usize;
-
-                let x = x.max(1).min(width - 2);
-                let y = y.max(1).min(height - 2);
-
-                d[[i, j]] = d0[[x, y]];
-            }
-        }
-    }
-
-    fn step(&mut self, dt: f64) {
-        let mut velocity_x0 = self.velocity_x.clone();
-        let mut velocity_y0 = self.velocity_y.clone();
-        let mut density0 = self.density.clone();
-    
-        self.diffuse(&mut self.velocity_x, &mut velocity_x0, self.viscosity, dt);
-        self.diffuse(&mut self.velocity_y, &mut velocity_y0, self.viscosity, dt);
-    
-        self.advect(&mut self.velocity_x, &velocity_x0, &velocity_x0, &velocity_y0, dt);
-        self.advect(&mut self.velocity_y, &velocity_y0, &velocity_x0, &velocity_y0, dt);
-    
-        self.diffuse(&mut self.density, &density0, self.diffusion, dt);
-        self.advect(&mut self.density, &density0, &self.velocity_x, &self.velocity_y, dt);
-    }
 }
 
 impl App {
@@ -149,8 +57,6 @@ impl App {
     fn update(&mut self, args: &UpdateArgs) {
         self.time += args.dt; // Update time
 
-        
-
         // DEMO: Paint every pixel from top to bottom
         // self.frame_buffer.put_pixel(
         //     (self.time / args.dt) as u32 % self.frame_buffer.width(), 
@@ -172,13 +78,7 @@ impl App {
             let value = (self.fluid.density[[x as usize, y as usize]] * 255.0) as u8;
             *pixel = image::Rgba([value, value, value, 255]);
         }
-
     }
-
-    
-
-
-
 }
 
 fn main() {
@@ -198,16 +98,13 @@ fn main() {
     // Create screen texture that is rendered
     let mut screen_texture = Texture::from_image(&frame_buffer, &TextureSettings::new().mag(Filter::Nearest));
 
-    // Create fluid instance
-    let mut fluid = Fluid::new((WINDOW_WIDTH / PIXEL_SCALE) as usize, (WINDOW_HEIGHT / PIXEL_SCALE) as usize, 0.1, 0.001);
-
     // Create app object
     let mut app = App {
         gl: GlGraphics::new(opengl),
         frame_buffer,
         time: 0.0,
         mouse_pos: [0.0, 0.0],
-        fluid,
+        fluid: Fluid::new(0.1, 0.001),
     };
 
     // Event loop
