d8bac0b269c6246841302ee79d707c8fc6904909
Benjaneb <71073919+Benjaneb@users.noreply.github.com>
2023-05-10 04:14:43 +0200

    Use multiple render targets

--- src/fluid_dynamics.frag
+++ src/fluid_dynamics.frag
@@ -9,8 +9,12 @@ uniform float diffusion;
 uniform float viscosity;
 
 out vec4 o_Color;
+out vec4 o_density;
+out vec4 o_velocity;
 
 void main() {
-    // o_Color = vec4(gl_FragCoord.xy / resolution, 0.0, 0.0);
-    o_Color = texture2D(density, gl_FragCoord.xy);
+    o_Color = vec4(gl_FragCoord.xy / resolution, 0.0, 0.0);
+    // vec4 current_density = texture(density, gl_FragCoord.xy / resolution);
+    // o_Color = vec4(current_density.xyz, 1.0);
+    o_density = vec4(1.0);
 }
--- src/main.rs
+++ src/main.rs
@@ -3,29 +3,46 @@ extern crate piston_window;
 #[macro_use] extern crate gfx;
 extern crate gfx_device_gl;
 
-use piston_window::*;
-use gfx::{traits::FactoryExt, Factory, memory::{Bind, Usage}, handle::{ShaderResourceView, Sampler}};
+use piston_window::{*, texture::UpdateTexture};
+use gfx::{traits::FactoryExt, Factory, memory::{Bind, Usage, Typed}, handle, format, texture};
+use gfx_device_gl::Resources;
 
 // Dimensions of simulation
-const SIM_WIDTH: usize = 16;
-const SIM_HEIGHT: usize = 16;
+const SIM_WIDTH: usize = 500;
+const SIM_HEIGHT: usize = 500;
 
-const PIXEL_SCALE: usize = 32; // How big every pixel should be (1 is normal)
+const PIXEL_SCALE: usize = 1; // How big every pixel should be (1 is normal)
 const WINDOW_WIDTH: u32 = (SIM_WIDTH * PIXEL_SCALE) as u32;
 const WINDOW_HEIGHT: u32 = (SIM_HEIGHT * PIXEL_SCALE) as u32;
+
+type Texture4f = handle::Texture<Resources, format::R32_G32_B32_A32>;
 
 pub struct App {
     mouse_pos: [f64; 2],
     mouse_movement: [f64; 2],
     left_mouse_state: ButtonState,
-    data: pipe::Data<gfx_device_gl::Resources>, // Data shared with shaders
+    data: pipe::Data<Resources>, // Data shared with shaders
+    density: [Texture4f; 2], // Output buffer & previous values
+    velocity: [Texture4f; 2],
 }
 
 impl App {
-    fn render(&mut self, args: &RenderArgs) {
+    fn render(&mut self, args: &RenderArgs, window: &mut PistonWindow) {
         // Store time between rendered frames (dt in UpdateArgs uses time between monitor frames wtf)
         self.data.dt = args.ext_dt as f32;
 
+        let img_info = texture::ImageInfoCommon {
+            xoffset: 0, yoffset: 0, zoffset: 0,
+            width: SIM_WIDTH as u16, height: SIM_HEIGHT as u16,
+            depth: 0,
+            format: format::Format(format::SurfaceType::R32_G32_B32_A32, format::ChannelType::Float),
+            mipmap: 0,
+        };
+        window.encoder.copy_texture_to_texture_raw(
+            self.density[0].raw(), None, img_info, 
+            self.density[1].raw(), None, img_info
+        ).unwrap();
+        
         // TODO: GUI rendering
     }
 
@@ -38,10 +55,43 @@ impl App {
             if x < SIM_WIDTH && y < SIM_HEIGHT {
                 // self.fluid.add_density(x, y, 0.2);
                 // self.fluid.add_velocity(x, y, self.mouse_movement[0] / 10.0, self.mouse_movement[1] / 10.0);
-                window.encoder.update_texture(, cube_face, img, data);
+                update_pixel(window, &self.density[1], x, y, [1.0 as u32; 4]);
             }
         }
     }
+}
+
+fn update_pixel(window: &mut PistonWindow, texture: &Texture4f, x: usize, y: usize, data: [u32; 4]) {
+    let img_info = texture::ImageInfoCommon {
+        xoffset: x as u16,
+        yoffset: y as u16,
+        zoffset: 0,
+        width: 1,
+        height: 1,
+        depth: 0,
+        format: (),
+        mipmap: 0,
+    };
+    window.encoder.update_texture::<_, format::Rgba32F>(&texture, None, img_info, &[data]).unwrap();
+}
+
+// For storing data unique to every pixel
+fn create_texture(window: &mut PistonWindow, usage: Usage, bind: Bind)
+    -> (Texture4f, handle::ShaderResourceView<Resources, [f32; 4]>, handle::Sampler<Resources>)
+{
+    use format::*;
+    use texture::*;
+    let kind = Kind::D2(SIM_WIDTH as u16, SIM_HEIGHT as u16, AaMode::Single);
+
+    let texture = window.factory.create_texture::<R32_G32_B32_A32>(
+        kind, 1, bind, usage, Some(ChannelType::Float)).unwrap();
+
+    let resource_view = window.factory.view_texture_as_shader_resource::<Rgba32F>(
+        &texture, (0, 0), Swizzle::new()).unwrap();
+    
+    let texture_info = SamplerInfo::new(FilterMethod::Scale, WrapMode::Clamp);
+    
+    return (texture, resource_view, window.factory.create_sampler(texture_info))
 }
 
 gfx_defines!(
@@ -51,32 +101,19 @@ gfx_defines!(
 
     pipeline pipe {
         vertex_buffer: gfx::VertexBuffer<Vertex> = (),
-        out_color: gfx::RenderTarget<gfx::format::Srgba8> = "o_Color",
+
         resolution: gfx::Global<[u32; 2]> = "resolution",
         dt: gfx::Global<f32> = "dt",
         density: gfx::TextureSampler<[f32; 4]> = "density",
         velocity: gfx::TextureSampler<[f32; 4]> = "velocity",
         diffusion: gfx::Global<f32> = "diffusion",
         viscosity: gfx::Global<f32> = "viscosity",
+
+        out_color: gfx::RenderTarget<format::Srgba8> = "o_Color",
+        out_density: gfx::RenderTarget<format::Rgba32F> = "o_density",
+        out_velocity: gfx::RenderTarget<format::Rgba32F> = "o_velocity",
     }
 );
-
-// For storing data unique to every pixel
-fn create_texture(window: &mut PistonWindow)
-    -> (ShaderResourceView<gfx_device_gl::Resources, [f32; 4]>, Sampler<gfx_device_gl::Resources>)
-{
-    let kind = gfx::texture::Kind::D2(SIM_WIDTH as u16, SIM_HEIGHT as u16, gfx::texture::AaMode::Single);
-
-    let texture = window.factory.create_texture::<gfx::format::R32_G32_B32_A32>(kind, 1, 
-        Bind::SHADER_RESOURCE.union(Bind::UNORDERED_ACCESS), Usage::Dynamic, Some(gfx::format::ChannelType::Float)).unwrap();
-
-    let resource_view = window.factory.view_texture_as_shader_resource::<(gfx::format::R32_G32_B32_A32, gfx::format::Float)>(
-        &texture, (0, 0), gfx::format::Swizzle::new()).unwrap();
-    
-    let texture_info = gfx::texture::SamplerInfo::new(gfx::texture::FilterMethod::Scale, gfx::texture::WrapMode::Clamp);
-
-    return (resource_view, window.factory.create_sampler(texture_info))
-}
 
 fn main() {
     let opengl = OpenGL::V3_2; // Change this to OpenGL::V2_1 if not working
@@ -91,7 +128,8 @@ fn main() {
     
     // Import shader files
     let pso = window.factory.create_pipeline_simple(
-        include_bytes!("basic.vert"), include_bytes!("fluid_dynamics.frag"), pipe::new()).unwrap();
+        include_bytes!("basic.vert"), include_bytes!("fluid_dynamics.frag"), pipe::new())
+        .expect("Failed to compile shaders");
     
     // Create vertex buffer with two triangles covering the screen
     // i.e. every pixel gets rendered with the fragment shader
@@ -106,20 +144,26 @@ fn main() {
         0, 1, 3
     ];
     let (vertex_buffer, slice) = window.factory.create_vertex_buffer_with_slice(&SCREEN_VERTICES, SCREEN_INDICES);
+
+    // Create textures for storing per-pixel data
+    let out_density_handles = create_texture(&mut window, Usage::Data, Bind::SHADER_RESOURCE | Bind::RENDER_TARGET | Bind::TRANSFER_SRC);
+    let out_velocity_handles = create_texture(&mut window, Usage::Data, Bind::SHADER_RESOURCE | Bind::RENDER_TARGET | Bind::TRANSFER_SRC);
     
-    // let (_, velocity_texture) = window.factory.create_texture_immutable::<gfx::format::Rgba32F>(kind, 
-    //     gfx::texture::Mipmap::Provided, &[&[[0u32; 4]; SIM_WIDTH * SIM_HEIGHT]]).unwrap();
+    let density_handles = create_texture(&mut window, Usage::Dynamic, Bind::SHADER_RESOURCE | Bind::TRANSFER_DST);
+    let velocity_handles = create_texture(&mut window, Usage::Dynamic, Bind::SHADER_RESOURCE | Bind::TRANSFER_DST);
     
-    // Crete pipeline data object
+    // Create pipeline data object
     let data = pipe::Data {
         vertex_buffer,
-        out_color: window.output_color.clone(),
         resolution: [WINDOW_WIDTH as u32, WINDOW_HEIGHT as u32],
         dt: 0.0,
-        density: create_texture(&mut window),
-        velocity: create_texture(&mut window),
+        density: (density_handles.1, density_handles.2),
+        velocity: (velocity_handles.1, velocity_handles.2),
         diffusion: 0.1,
         viscosity: 0.001,
+        out_color: window.output_color.clone(),
+        out_density: window.factory.view_texture_as_render_target(&out_density_handles.0, 0, None).unwrap(),
+        out_velocity: window.factory.view_texture_as_render_target(&out_velocity_handles.0, 0, None).unwrap(),
     };
 
     // Create app object
@@ -128,21 +172,23 @@ fn main() {
         mouse_movement: [0.0, 0.0],
         left_mouse_state: ButtonState::Release,
         data,
+        density: [out_density_handles.0, density_handles.0],
+        velocity: [out_velocity_handles.0, velocity_handles.0],
     };
     
     // Event loop
     let mut events = Events::new(EventSettings::new());
     while let Some(e) = events.next(&mut window) {
-        if let Some(args) = e.render_args() {
-            app.render(&args);
-        }
         window.draw_3d(&e, |window| {
             window.encoder.clear(&window.output_color, [0.0, 0.0, 0.0, 1.0]);
             window.encoder.draw(&slice, &pso, &app.data);
         });
+        if let Some(args) = e.render_args() {
+            app.render(&args, &mut window);
+        }
 
         if let Some(args) = e.update_args() {
-            app.update(&args, &window);
+            app.update(&args, &mut window);
         }
 
         if let Some(pos) = e.mouse_cursor_args() {
